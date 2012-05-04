e33835f9d5adc969b8f884bcdce0695f4b215aa0
Greg Allen <GregoryEAllen@gmail.com>
2012-05-03 23:30:58 -0500

    common.mk test files are all gz, generating pass/fail results

--- test/common.mk
+++ test/common.mk
@@ -1,9 +1,36 @@
+# This file is part of MLPolyGen, a maximal-length polynomial generator
+# for linear feedback shift registers.
+# 
+# Copyright (C) 2012  Gregory E. Allen
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# This makfile (along with helper python script mlpolyN.py) can make
+# an output file of a given order (e.g. 32) as follows:
+## cd mlpolygen/test
+## make -f mlpoly32.txt
+# The file mlpoly32.mk will be generated (by the python script),
+# containing rules to generate mlpoly32.txt.
+
 MLPOLYGEN=../build/mlpolygen
 
 mlpoly%.mk : mlpolyN.py common.mk
 	python $< $* > $@
 
-mlpoly%.txt : mlpoly%.mk
+mlpoly%.txt.gz : mlpoly%.mk
+	mkdir -p files
 	$(MAKE) -f $^
 
 # make sure that shell brace expansion works, e.g. {1..20}
@@ -12,18 +39,48 @@ SHELL=/bin/bash
 TEST_MAX ?= 24
 
 TEST_SIZES = $(shell echo {1..$(TEST_MAX)})
-TEST_FILES = $(patsubst %, mlpoly%.txt, $(TEST_SIZES))
+TEST_FILES = $(patsubst %, mlpoly%.txt.gz, $(TEST_SIZES))
 TEST_MKFS = $(patsubst %, mlpoly%.mk, $(TEST_SIZES))
 
-test: $(TEST_FILES)
-	wc -l $^
+test: results.txt $(TEST_FILES)
+	@cat $<
 
 clean: $(TEST_MKFS)
 	for file in $^; do $(MAKE) -f $$file clean-files; done
+	rm files lengths*.txt
 
-# This makfile (along with helper python script mlpolyN.py) can make
-# an output file of a given order (e.g. 32) as follows:
-## cd mlpolygen/test
-## make -f mlpoly32.txt
-# The file mlpoly32.mk will be generated (by the python script),
-# containing rules to generate mlpoly32.txt.
+gzcheck: # check that all .gz files are valid
+	find files -name mlpoly\*.txt.gz -print0 | xargs -0 gzcat -t
+
+# a helpful function
+sort_gzipped = sort $(foreach f,$(1),<(gzcat $(f)))
+
+# everything below here is to verify the sequence lengths and generate results.txt
+files/lengths.raw.txt : $(TEST_FILES)
+	@echo making $@
+	@printf '' > $@
+	@for f in $^; do gzcat $$f | wc -l >> $@; done
+
+files/lengths.txt : files/lengths.raw.txt
+	@echo making $@
+	@order=1; while read line; do echo $$order $$line; order=$$((order+1)); done < $< > $@
+
+# get some published answers for the sequence lengths
+files/lengths.ref.txt :
+	@echo downloading $@
+	@curl http://oeis.org/A011260/b011260.txt | grep -v '#' > $@
+
+comm23_unsorted = comm -23 $(foreach f,$(1),<(sort $(f)))
+
+files/lengths.err.txt : files/lengths.txt files/lengths.ref.txt
+	@echo making $@
+	@$(call comm23_unsorted,$^) > $@
+
+results.txt : files/lengths.err.txt cleanresults
+	@STR="sequence lengths 1 to $(TEST_MAX):"; \
+	if [ -s $< ]; then \
+      echo "$$STR FAILED" > $@; cat $< >> $@; \
+    else echo "$$STR passed" >> $@; fi
+
+cleanresults :
+	@rm -f results.txt
--- test/mlpolyN.py
+++ test/mlpolyN.py
@@ -1,4 +1,23 @@
 #/usr/bin/env python
+# This file is part of MLPolyGen, a maximal-length polynomial generator
+# for linear feedback shift registers.
+# 
+# Copyright (C) 2012  Gregory E. Allen
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 # This file makes makefiles that can be used to generate a mlpoly
 # file of a specified order.
 # It splits the output space into as many as 2**16 subspaces
@@ -12,14 +31,13 @@
 import sys
 
 def SegTargetList(polyOrd,totSegOrd,segOrd):
-	if segOrd==0:
-		'root name'
-		return ['mlpoly%d.txt' % polyOrd]
+	if segOrd==0: # root name
+		return ['mlpoly%d.txt.gz' % polyOrd]
 	totNumDigits = (totSegOrd+3)/4
 	numXs = (totSegOrd-segOrd)/4
 	numDigits = totNumDigits-numXs
 	assert (totSegOrd==segOrd) or (totSegOrd-segOrd>=4)
-	fmtStr = 'files/mlpoly%d-%%0%dx%s.txt' % (polyOrd, numDigits, 'x'*numXs)
+	fmtStr = 'files/mlpoly%d-%%0%dx%s.txt.gz' % (polyOrd, numDigits, 'x'*numXs)
 	incr = 2**((totSegOrd-segOrd)%4)
 	return [ fmtStr%(x*incr) for x in range(2**segOrd) ]
 
@@ -27,20 +45,20 @@ def PrintSortRules(olist,ilist):
 	leno = len(olist)
 	leni = len(ilist)
 	entriesPerRule = leni/leno
-	print '# %d sort rule(s) of %d files each' % (leno,entriesPerRule)
+	print '\n# %d sort rule(s) of %d files each' % (leno,entriesPerRule)
 	for x in xrange(leno):
 		print '%s:' % olist[x],
 		subList = ilist[ x*entriesPerRule : x*entriesPerRule+entriesPerRule ]
 		print ' '.join(subList)
-		print '\tsort $^ > $@'
+		print '\t$(call sort_gzipped,$^) | gzip > $@'
 
 def PrintLeafRules(order, olist):
 	numSegments = len(olist)
-	print '# %d leaf rule(s)' % (numSegments)
+	print '\n# %d leaf rule(s)' % (numSegments)
 
 	if numSegments==1: # no need for sval, eval, don't want -p
 		print '%s:' % olist[0]
-		print '\t$(MLPOLYGEN) %d > $@' % order
+		print '\t$(MLPOLYGEN) %d | gzip -c > $@' % order
 		return
 
 	totalEndValue = 2**order # actually +1
@@ -51,7 +69,8 @@ def PrintLeafRules(order, olist):
 		sval = totalStartValue+s*incr
 		eval = sval+incr-1
 		print '%s:' % olist[s]
-		print '\t$(MLPOLYGEN) -p -s0x%x -e0x%x %d > $@' % (sval,eval,order)
+		print '\t$(MLPOLYGEN) -p -s0x%x -e0x%x %d \\' % (sval,eval,order)
+		print '    | gzip > $@' 
 
 def main():
 	order = int(sys.argv[1])
@@ -75,9 +94,9 @@ def main():
 				segmentsOrder = 16
 			numLevels = segmentsOrder/9 + 1
 			incrPerLevel = (segmentsOrder/numLevels) + 1
-			print '# %d levels, %d incrPerLevel' % (numLevels,incrPerLevel)
+			print '# %d levels, %d incrPerLevel\n' % (numLevels,incrPerLevel)
 	
-	print 'mlpoly%d.txt:' % order
+	print 'mlpoly%d.txt.gz:' % order
 	print '-include common.mk'
 
 	currSegOrder = 0
@@ -94,8 +113,8 @@ def main():
 	PrintLeafRules(order,currTgtList)
 
 	print 'clean-mlpoly%d:' % order
-	print '\tfind files -name mlpoly%d-*.txt -print0 | xargs -0 rm' % order
-	print '\trm -f mlpoly%d.txt' % order
+	print '\tfind files -name mlpoly%d-*.txt.gz -print0 | xargs -0 rm' % order
+	print '\trm -f mlpoly%d.txt.gz' % order
 
 	print 'clean-files:: clean-mlpoly%d' % order
 
